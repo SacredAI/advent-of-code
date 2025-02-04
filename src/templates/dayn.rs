8ee561d6208b29c9da0822dab5fa137a76260d72
Alex <alex.donn@hotmail.com>
2025-02-04 14:41:03 +1000

    Template update Day 10

--- src/templates/dayn.rs
+++ src/templates/dayn.rs
@@ -4,10 +4,10 @@ pub fn parse(input: &str) -> Input {
     0
 }
 
-pub fn part1(input: Input) -> u32 {
+pub fn part1(input: &Input) -> u32 {
     0
 }
 
-pub fn part2(input: Input) -> u32 {
+pub fn part2(input: &Input) -> u32 {
     0
 }
